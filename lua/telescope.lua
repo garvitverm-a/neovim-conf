76f61ea65737f1127e93eee2b404ffaf4d5434ce
garvitverm-a <garvit.verma2512@gmail.com>
2025-08-01 01:08:55 +0530

    updated conflicting keybinds

--- lua/telescope.lua
+++ lua/telescope.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<CR>", { desc = "Find files using Telescope" })
-vim.keymap.set('n', '<leader>fm', "<cmd>Telescope marks<CR>", { desc = "Find marks created using Telescope" })
+vim.keymap.set('n', '<leader>sf', "<cmd>Telescope find_files<CR>", { desc = "Find files using Telescope" })
+vim.keymap.set('n', '<leader>sm', "<cmd>Telescope marks<CR>", { desc = "Find marks created using Telescope" })
